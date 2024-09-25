6f9c116c43bf932b29ec20817951953efc01fdb2
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-24 22:29:03 -0300

    add: sendEmail service

--- main.js
+++ main.js
@@ -1,46 +1,5 @@
-import emailJS from '@emailjs/browser'
-
+import { sendEmail } from "./src/services/sendEmail";
 const contactForm = document.getElementById("contact-form")
-
-const key = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-console.log(key)
-emailJS.init({
-  publicKey: key
-})
-
-let template = {
-  to_name: 'Andres',
-  from_name: '',
-  subject: 'Folium',
-  message: 'teste',
-  email: ''
-};
-
-const clearForm = () => {
-  contactForm.reset()
-}
-
-const sendEmail = () => {
-  const name = document.getElementById("name")
-  const message = document.getElementById("message")
-  const email = document.getElementById("email")
-
-  template.from_name = name.value
-  template.message = message.value
-  template.email = email.value
-
-  emailJS.send('service_9564nrp', 'template_w2vkrv8', template)
-    .then((res) => {
-      if (res.status === 200) {
-        alert("Email enviado!")
-        clearForm()
-      }
-    }),
-    (error) => {
-      console.log('error' + error)
-      alert('Ocorreu um erro: ' + error)
-    }
-}
 
 contactForm.addEventListener('submit', (event) => {
   event.preventDefault();
---
+++ src/services/sendEmail.js
@@ -0,0 +1,43 @@
+import emailJS from '@emailjs/browser'
+
+const contactForm = document.getElementById("contact-form")
+
+const key = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+console.log(key)
+emailJS.init({
+  publicKey: key
+})
+
+let template = {
+  to_name: 'Andres',
+  from_name: '',
+  subject: 'Folium',
+  message: 'teste',
+  email: ''
+};
+
+export const sendEmail = () => {
+  const name = document.getElementById("name")
+  const message = document.getElementById("message")
+  const email = document.getElementById("email")
+
+  template.from_name = name.value
+  template.message = message.value
+  template.email = email.value
+
+  emailJS.send('service_9564nrp', 'template_w2vkrv8', template)
+    .then((res) => {
+      if (res.status === 200) {
+        alert("Email enviado!")
+        clearForm()
+      }
+    }),
+    (error) => {
+      console.log('error' + error)
+      alert('Ocorreu um erro: ' + error)
+    }
+}
+
+const clearForm = () => {
+  contactForm.reset()
+}
